284faa978858dc0fb1ab1c770b497c08de63751d
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/commandline_it.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Run in terminal</source>
+        <translation>Esegui nel terminale</translation>
+    </message>
+    <message>
+        <source>Run in terminal and close on exit</source>
+        <translation>Esegui nel terminale e chiudi all&apos;uscita</translation>
+    </message>
+    <message>
+        <source>Run in background (without terminal)</source>
+        <translation>Esegui in background (senza terminale)</translation>
+    </message>
+    <message>
+        <source>Run &apos;%1&apos;</source>
+        <translation>Esegui &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>I&apos;m Feeling Lucky</source>
+        <translation>Sono fortunato</translation>
+    </message>
+    <message>
+        <source>Try running &apos;%1&apos;</source>
+        <translation>Prova a eseguire &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <source>&lt;command&gt; [params]</source>
+        <translation>&lt;comando&gt; [parametri]</translation>
+    </message>
+</context>
+</TS>
